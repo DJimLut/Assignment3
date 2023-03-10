ec162159bb455192a3ec5c7572563f7cfb8c4667
Mason Richardson <47069748+srichardson22@users.noreply.github.com>
2023-03-09 18:22:14 -0600

    Implement DP
    Implement dynamic programming solution

--- src/main/CoinChange.java
+++ src/main/CoinChange.java
@@ -6,20 +6,54 @@
 
 package main;
 
+import java.util.Arrays;
+
 import static java.util.Arrays.*;
 
 public class CoinChange {
-    public static int coinChangeDP( int[] coinValues, int change ) {
-        // TODO: Implement method
-        throw new UnsupportedOperationException();
-    }
 
     /**
      * @param coinValues is the coin denominations
      * @param change is the amount of change needed
      * @return the number of coins needed to give correct change
      */
-    public static int coinChangeGreedy( int[] coinValues, int change ) {
+    public static int coinChangeDP(int[] coinValues, int change) {
+        //Check if change is negative or 0
+        if (change <= 0) {
+            return 0;
+        }
+        //Check for negative values in coinValues
+        for (int i = 0; i < coinValues.length; i++) {
+            if (coinValues[i] < 0) {
+                return 0;
+            }
+        }
+
+        //Calculate problem
+        int stored[] = new int[change + 1]; //Create array to store already found values
+        Arrays.fill(stored, Integer.MAX_VALUE); //Set array values as infinite
+        stored[0] = 0; //Base case
+
+        for (int i = 1; i <= change; i++) {
+            for (int j = 0; j < coinValues.length; j++) {
+                if (coinValues[j] <= i) {
+                    int temp = stored[i - coinValues[j]];
+                    if(temp != Integer.MAX_VALUE && (temp + 1) < stored[i]){
+                        stored[i] = temp + 1;
+                    }
+                }
+            }
+        }
+        return stored[change];
+    }
+
+
+    /**
+     * @param coinValues is the coin denominations
+     * @param change is the amount of change needed
+     * @return the number of coins needed to give correct change
+     */
+    public static int coinChangeGreedy (int[] coinValues, int change ) {
         int coins = 0;
         sort( coinValues );
         for( int i = coinValues.length - 1; i >= 0; i-- ) {
